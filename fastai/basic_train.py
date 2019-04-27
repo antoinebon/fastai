ff15173b8528834dabd8024eb6f35e5c5aff4274
Antoine Bon <antoine.bon@jumio.com>
2019-04-27 17:26:28 +0200

    fixed typos

--- fastai/basic_train.py
+++ fastai/basic_train.py
@@ -363,7 +363,7 @@ class Learner():
         "Return predicted class, label and probabilities for `item`."
         batch = self.data.one_item(item)
         res = self.pred_batch(batch=batch)
-        raw_pred,x = grab_idx(res,0,batch_first),batch[0]
+        raw_pred,x = grab_idx(res,0,batch_first=batch_first),batch[0]
         norm = getattr(self.data,'norm',False)
         if norm:
             x = self.data.denorm(x)
@@ -371,7 +371,7 @@ class Learner():
         ds = self.data.single_ds
         pred = ds.y.analyze_pred(raw_pred, **kwargs)
         x_item = ds.x.reconstruct(x[0])
-        y_item = ds.y.reconstruct(out_pred, x_item) if has_arg(ds.y.reconstruct, 'x') else ds.y.reconstruct(out_pred)
+        y_item = ds.y.reconstruct(pred, x_item) if has_arg(ds.y.reconstruct, 'x') else ds.y.reconstruct(pred)
         return x_item, y_item, pred, raw_pred
 
     def validate(self, dl=None, callbacks=None, metrics=None):
